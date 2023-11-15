a8132d9f2505ab34e8e6097e65175f09950bf182
Nathan Rydel <nathanrydel@gmail.com>
2023-11-14 18:38:31 -0600

    Added JS TODOs

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Let's get this party started!");
+// TODO: Create variables
+// apiKey
+// URL
+// $formInput
+// $gifContainer
+// $removeButton
+
+// TODO: Grab the search term from the DOM
+
+// TODO: Create a form submission listener
+
+// TODO: Handle form submission
+
+// TODO: Append the GIF to the DOM
+
+// TODO: Remove all GIFs from the DOM
