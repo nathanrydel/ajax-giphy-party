5c7ca8cc2234f9c539775c6405371c1fe3ac1364
Carl Molina <carlandrew.molina@gmail.com>
2023-11-14 17:00:13 -0800

    Added searchGiphy function

--- app.js
+++ app.js
@@ -12,9 +12,25 @@ const $removeButton = $("#remove-btn");
 
 // TODO: Create a form submission listener to Grab the search term from the DOM
 // On submission get the val from the field
+$formInput.on("submit", getFormInput);
+
+function getFormInput(evt) {
+  evt.preventDefault();
+  const $formVal = $formInput.val();
+  searchGiphy($formVal);
+}
 
 // TODO: Search GIPHY for the GIF
+
+async function searchGiphy(searchTerm) {
+  const searchParam = new URLSearchParams({searchTerm});
+  const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${searchParam}&api_key=${apiKey}`);
+  const gif = await response.text();
+
+}
+
 
 // TODO: Append the GIF to the DOM
 
 // TODO: Remove all GIFs from the DOM
+
